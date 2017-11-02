c9f99be3fa2a841d4966f361a2d02899c5487843
Daniel Haro <contato.danielharo@gmail.com>
2017-11-02 21:16:06 -0200

    Projeto que está criado no curso de Asp.Net Mvc com Entity FrameWork

---
+++ SistemaLoja/Context/LojaContext.cs
@@ -0,0 +1,15 @@
+﻿using SistemaLoja.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLoja.Context
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto>Produto { get; set; }
+
+    }
+}
---
+++ SistemaLoja/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLoja.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public float Estoque { get; set; }
+        public string Descricao { get; set; }
+
+        public DateTime UltimaCompra { get; set; }
+    }
+}
