545a49a00d3f842057d907013ad6a0528a15167d
Daniel Haro <contato.danielharo@gmail.com>
2017-11-13 22:35:51 -0200

    Programando as opcoes de Cliente

--- SistemaLoja/Controllers/OrdensController.cs
+++ SistemaLoja/Controllers/OrdensController.cs
@@ -30,10 +30,66 @@ namespace SistemaLoja.Controllers
         [HttpPost]
         public ActionResult NovaOrdem(OrdemView ordemView)
         {
+            ordemView = Session["ordemView"] as OrdemView;
+            var customizarId = int.Parse(Request["CustomizarId"]);
             var list = db.Customizars.ToList();
-            list.Add(new Customizar { CustomizarId = 0, Nome = "[Selecione o tipo de Documento]" });
-            list = list.OrderBy(c => c.NomeCompleto).ToList();
-            ViewBag.CustomizarId = new SelectList(list, "CustomizarId", "NomeCompleto");
+
+            if (customizarId == 0)
+            {
+                list = db.Customizars.ToList();
+                list.Add(new Customizar { CustomizarId = 0, Nome = "[Selecione um Cliente]" });
+                list = list.OrderBy(c => c.NomeCompleto).ToList();
+                ViewBag.CustomizarId = new SelectList(list, "CustomizarId", "NomeCompleto");
+                ViewBag.Error = "Selecione um cliente";
+
+
+
+                return View(ordemView);
+
+            }
+
+            var cliente = db.Customizars.Find(customizarId);
+            if (cliente == null)
+            {
+
+                list = db.Customizars.ToList();
+                list.Add(new Customizar { CustomizarId = 0, Nome = "[Selecione um Cliente]" });
+                list = list.OrderBy(c => c.NomeCompleto).ToList();
+                ViewBag.CustomizarId = new SelectList(list, "CustomizarId", "NomeCompleto");
+                ViewBag.Error = "Selecione um cliente";
+
+                return View(ordemView);
+            }
+
+            if (ordemView.Produtos.Count == 0)
+            {
+                list = db.Customizars.ToList();
+                list.Add(new Customizar { CustomizarId = 0, Nome = "[Selecione um Cliente]" });
+                list = list.OrderBy(c => c.NomeCompleto).ToList();
+                ViewBag.CustomizarId = new SelectList(list, "CustomizarId", "NomeCompleto");
+                ViewBag.Error = "Selecione um cliente";
+                return View(ordemView);
+            }
+            var ordem = new Ordem
+            {
+                CustomizarId = customizarId,
+                OrdemData = DateTime.Now,
+                OrdemStatus = OrdemStatus.Criada
+            };
+
+            db.Ordem.Add(ordem);
+            db.SaveChanges();
+
+            var ordemId = db.Ordem.ToList().Select(o => o.OrdemId).Max();
+            foreach (var item in ordemView.Produtos)
+            {
+                var ordemDetalhes = new OrdemDetalhe
+                {
+                    Descricao = item.Descricao,
+                    Quantidade = item.Quantidade,
+                    Preco = item.Preco
+                };
+            }
             return View(ordemView);
 
         }
@@ -51,6 +107,7 @@ namespace SistemaLoja.Controllers
         [HttpPost]
         public ActionResult AddProduto(ProdutoOrdem produtoOrdem)
         {
+
             var ordemView = Session["ordemView"] as OrdemView;
 
             var list = db.Produtoes.ToList();
@@ -77,7 +134,7 @@ namespace SistemaLoja.Controllers
                 list.Add(new ProdutoOrdem { ProdutoId = 0, Descricao = "[Selecione o tipo de Documento]" });
                 list = list.OrderBy(c => c.Descricao).ToList();
                 ViewBag.ProdutoId = new SelectList(list, "ProdutoId", "Descricao");
-                ViewBag.Error = "Erro";
+                ViewBag.Error = "Não existe o produto selecionado";
 
                
 
@@ -85,6 +142,13 @@ namespace SistemaLoja.Controllers
 
 
             }
+
+            produtoOrdem = ordemView.Produtos.Find(p => p.ProdutoId == produtoId);
+            if(produtoOrdem == null )
+            {
+
+            
+
             produtoOrdem = new ProdutoOrdem
             {
                 Descricao = produto.Descricao,
@@ -94,6 +158,10 @@ namespace SistemaLoja.Controllers
             };
 
             ordemView.Produtos.Add(produtoOrdem);
+            }else
+            {
+                produtoOrdem.Quantidade += float.Parse(Request["Quantidade"]);
+            }
             var listC = db.Customizars.ToList();
             listC.Add(new Customizar { CustomizarId = 0, Nome = "[Selecione o tipo de Documento]" });
             listC = listC.OrderBy(c => c.NomeCompleto).ToList();
--- SistemaLoja/Models/SistemaLojaContext.cs
+++ SistemaLoja/Models/SistemaLojaContext.cs
@@ -33,5 +33,8 @@ namespace SistemaLoja.Models
         public System.Data.Entity.DbSet<SistemaLoja.Models.Fornecedor> Fornecedors { get; set; }
 
         public System.Data.Entity.DbSet<SistemaLoja.Models.Customizar> Customizars { get; set; }
+        public System.Data.Entity.DbSet<SistemaLoja.Models.Ordem> Ordem { get; set; }
+
+        public System.Data.Entity.DbSet<SistemaLoja.Models.OrdemDetalhe> OrdemDetalhe { get; set; }
     }
 }
